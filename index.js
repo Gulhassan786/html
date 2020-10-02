243ec3e09929c4fa024c2dc14ae2ebd03f244fb5
Gulhassan786 <gulhassanh49@gmail.com>
2020-10-02 11:42:35 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
-alert("how are you hassan")
-console.log("Hi! I am hassan a devloper")
+alert("how are you hassan");
+
+console.log("Hi! I am hassan a devloper");
+console.log("Hi! I am hassan a devloper");
+console.log("Hi! I am hassan a devloper");
+console.log("Hi! I am hassan a devloper");
