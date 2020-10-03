6da078ddbe97ab454e1939f1e89a420568ed45e2
Gulhassan786 <gulhassanh49@gmail.com>
2020-10-03 23:52:31 +0500

    new changes in index.html and index.js

--- index.js
+++ index.js
@@ -1,6 +1,1 @@
 alert("how are you noor hussain From gul hassan :) ");
-
-console.log("Hi! I am hassan a devloper");
-console.log("Hi! I am hassan a devloper");
-console.log("Hi! I am hassan a devloper");
-console.log("Hi! I am hassan a devloper");
